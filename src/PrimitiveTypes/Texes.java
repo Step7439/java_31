f9922dd09e5a2da80c911573aac23e99c629237a
Stepan <zamashikovs@mail.ru>
2022-01-26 20:08:26 +0900

    edit

--- src/PrimitiveTypes/Texes.java
+++ src/PrimitiveTypes/Texes.java
@@ -3,15 +3,14 @@ import java.util.Scanner;
 
 public class Texes {
     public static Scanner scanner = new Scanner(System.in);
-    public static int earnings = 0;
-    public static int spendings = 0;
-    public static String input;
 
-    public static int income(int earnings){
-        int tax = earnings * 8 / 100;
-        if(tax >= 0){
+
+    //public static String exit;
+    public static int income(int earnings) {
+        int tax = (int )(earnings * 0.06);
+        if (tax >= 0) {
             return tax;
-        }else {
+        } else {
             return 0;
         }
     }
@@ -23,16 +22,23 @@ public class Texes {
             return 0;
         }
     }
-
     public static void main(String[] args) {
-
-
+        int earnings = 0;
+        int spendings = 0;
+        //int obsIncome = 0;
+        //int obsIncomeAndExpense = 0;
         while(true) {
-            int operation = Integer.parseInt(input);
-            input = scanner.nextLine();
-            if ("end".equals(input)) {
+            System.out.println("Выберите операцию и введите её номер:\n" +
+                    "1. Добавить новый доход\n" +
+                    "2. Добавить новый расход\n" +
+                    "3. Выбрать ситему налогооблажения\n");
+            String exit = scanner.nextLine();
+            if ("end".equals(exit)) {
+                System.out.println("Программа завершена!");
                 break;
             }
+
+            int operation = Integer.parseInt(exit);
             switch (operation) {
                 case 1:
                     System.out.println("Введите сумму дохода: ");
@@ -47,12 +53,25 @@ public class Texes {
                     spendings += money1;
                     break;
                 case 3:
-                    //Показывает методы и сравневает значения
+                    int x = income(earnings);
+                    int c = incomeAndExpense(earnings, spendings);
+                    if(x <= c){
+                        System.out.println("Мы советуем вам УСН доходы:\n" +
+                                "Ваш налог составит: " + x + " рублей\n" +
+                                "Налог на другой системе: " + c + " рублей\n" +
+                                "Экономия: " + (c - x) + " рублей");
+                    }else {
+                        System.out.println("Мы советуем вам УСН доходы и расходы:\n" +
+                                "Ваш налог составит: " + c + " рублей\n" +
+                                "Налог на другой системе: " + x + " рублей\n" +
+                                "Экономия: " + (c - x) + " рублей");
+                        }
+                    System.out.println();
                     break;
                 default:
                     System.out.println("Такой оперций нет!");
-
-            }System.out.println("Программа завершена!");
+            }
         }
+
     }
 }
