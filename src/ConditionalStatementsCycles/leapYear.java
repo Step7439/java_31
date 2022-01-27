b0858622f16d8f5d82225e6449a742db198f279e
Stepan <zamashikovs@mail.ru>
2022-01-27 16:47:57 +0900

    edit

--- src/ConditionalStatementsCycles/leapYear.java
+++ src/ConditionalStatementsCycles/leapYear.java
@@ -6,11 +6,14 @@ public class leapYear {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите год в формате \"yyyy\"");
-        int yaar = scanner.nextInt();
+        int x1 = scanner.nextInt();
+        yaer(x1);
+    }
+    public static void yaer( int x){
 
-        if(yaar % 400 == 0 || yaar % 4 == 0){
+        if (x % 400 == 0 || x % 4 == 0) {
             System.out.println("Количество дней 366");
-        }else if (yaar % 100 != 0){
+        } else if (x % 100 != 0) {
             System.out.println("Количество дней 365");
         }
     }
