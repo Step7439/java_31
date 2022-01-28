f185c7e4b68b8ddea51e4dfa691a7aaf8728eedd
stepan <zamashikovs@mail.ru>
2022-01-28 14:45:54 +0900

    добавил метод

--- src/ConditionalStatementsCycles/leapYear.java
+++ src/ConditionalStatementsCycles/leapYear.java
@@ -3,18 +3,21 @@ package ConditionalStatementsCycles;
 import java.util.Scanner;
 
 public class leapYear {
+    public static int yaer(int x) {
+        if (x % 400 == 0 || x % 4 == 0) {
+            x = 366;
+        } else if (x % 100 != 0) {
+            x = 365;
+        }
+        return x;
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите год в формате \"yyyy\"");
         int x1 = scanner.nextInt();
-        yaer(x1);
+        int sum = yaer(x1);
+        System.out.println(sum);
     }
-    public static void yaer( int x){
 
-        if (x % 400 == 0 || x % 4 == 0) {
-            System.out.println("Количество дней 366");
-        } else if (x % 100 != 0) {
-            System.out.println("Количество дней 365");
-        }
-    }
+
 }
