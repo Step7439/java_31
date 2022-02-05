f98b8098266b7ad47f945cba0e49bc9989eb67b2
Stepan <zamashikovs@mail.ru>
2022-02-05 23:39:05 +0900

    Добавил поля

--- src/Numbers/Test.java
+++ src/Numbers/Test.java
@@ -4,9 +4,16 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        Games petay = new Games(4, "Petay");
-        Games vasy = new Games(7, "Vasy");
-        Games vova = new Games(0, "Vovka");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(">>");
+        int pley1 = scanner.nextInt();
+        Games petay = new Games(pley1, "Petay");
+        System.out.print(">>");
+        int pley2 = scanner.nextInt();
+        Games vasy = new Games(pley2, "Vasy");
+        System.out.print(">>");
+        int pley3 = scanner.nextInt();
+        Games vova = new Games(pley3, "Vovka");
 
         petay.logikGames();
         vasy.logikGames();
