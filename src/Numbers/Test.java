dd1135eb243bb860eb40a5a157f8a3dbb67bf566
stepan <zamashikovs@mail.ru>
2022-02-05 22:09:19 +0900

    Добавил поля

--- src/Numbers/Test.java
+++ src/Numbers/Test.java
@@ -3,12 +3,13 @@ package Numbers;
 import java.util.Scanner;
 
 public class Test {
-    /*public static void main(String[] args) {
-        Games petay = new Games(int 1);
-        Games vasy = new Games();
-        Games vova = new Games();
+    public static void main(String[] args) {
+        Games petay = new Games(4, "Petay");
+        Games vasy = new Games(7, "Vasy");
+        Games vova = new Games(0, "Vovka");
 
         petay.logikGames();
-
-    }*/
+        vasy.logikGames();
+        vova.logikGames();
+    }
 }
