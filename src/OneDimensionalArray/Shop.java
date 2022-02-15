c9f833c7222da2a62390035c76b53951eff94cf8
Stepan <zamashikovs@mail.ru>
2022-02-15 22:28:24 +0900

    Edit

--- src/OneDimensionalArray/Shop.java
+++ src/OneDimensionalArray/Shop.java
@@ -1,12 +1,10 @@
 package OneDimensionalArray;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Shop {
     public static String[] products = {"Хлеб", "Яблоки", "Молоко"};
-    public static int[] prices = {100, 200, 300};
+    public static int[] prices = {51, 250, 86};
     public static int[] product1 = new int[prices.length];
     public static int[] product2 = new int[prices.length];
     public static String[] product3 = new String[products.length];
@@ -35,10 +33,23 @@ public class Shop {
             int currentPrice = prices[productNumber];
             product1[productNumber] = currentPrice * productCount;
             product2[productNumber] = productCount;
-            product3[productNumber] = products[productNumber];
-                System.out.println(Arrays.toString(product3));
-
         }
+        System.out.println("Ваша корзина:");
+        System.out.println("Наименование товара: | Кол-во | Цена/за.ед | Общая стоимость");
+
+        int sum = 1;
+        for (int i = 0; i < products.length; i++) {
+            if (product1[i] > 0) {
+                product3[i] = sum + "." + products[i] + "               | " + product2[i] + "шт   | " + prices[i] + "руб      | " + product1[i] + "руб";
+                System.out.println(product3[i]);
+                sum++;
+            }
+        }
+        int sum1 = 0;
+        for(int i = 0; i < prices.length; i++){
+            sum1 += product1[i];
+        }
+        System.out.println("Итого: " + sum1);
     }
-    }
+}
 
